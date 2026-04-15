e8eb17744c61531dd6415022d7d7aab7f58dd826
Henry Alencar <henryalencar2007@gmail.com>
2026-04-15 13:59:54 -0300

    Update NavBar.jsx

--- PROJETO/Projeto/Projeto/src/components/NavBar/NavBar.jsx
+++ PROJETO/Projeto/Projeto/src/components/NavBar/NavBar.jsx
@@ -1,33 +1,14 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './NavBar.css';
 
 const NavBar = () => {
-  const navigate = useNavigate();
-
-  const scrollToSection = (id) => {
-    // Se não estiver na Home, primeiro navega para: /
-    if (window.location.pathname !== '/') {
-      navigate('/'); // navega para Home
-
-      // espera 200ms para o DOM carregar
-      setTimeout(() => {
-        const el = document.getElementById(id);
-        if (el) el.scrollIntoView({ behavior: 'smooth' });
-      }, 200);
-    } else {
-      const el = document.getElementById(id);
-      if (el) el.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-
   return (
     <nav>
       <ul className="nav-list">
-        <li><button onClick={() => scrollToSection('home')}>Home</button></li>
-        <li><button onClick={() => scrollToSection('sobre')}>Sobre</button></li>
-        <li><button onClick={() => scrollToSection('cards')}>Veículos</button></li>
-        <li><button onClick={() => scrollToSection('contato')}>Contato</button></li>
-        
+        <li><Link to="/#home">Home</Link></li>
+        <li><Link to="/#sobre">Sobre</Link></li>
+        <li><Link to="/#cards">Veículos</Link></li>
+        <li><Link to="/#contato">Contato</Link></li>
         <li><Link to="/login">Login</Link></li>
       </ul>
     </nav>
